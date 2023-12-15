2bec3683617be89534a7da918ecb92d16673859f
DCloud_LXH <283700113@qq.com>
2023-12-15 19:38:19 +0800

    feat: SimpleLayout

---
+++ layouts/SimpleLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="theme-container">
+    <Content class="theme-default-content" />
+  </div>
+</template>
+
+<script>
+import PageEdit from '@theme/components/PageEdit.vue'
+export default {
+  name: 'SimpleLayout',
+  components: {
+    PageEdit
+  }
+}
+</script>

1a80f77d39a3bddcdb3e203e9253dc48d1b81a69
DCloud_LXH <283700113@qq.com>
2026-03-25 11:12:26 +0800

    feat: 优化 navbar 样式

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -233,13 +233,6 @@ export default {
     }
   }
 }
-
-function css (el, property) {
-  // NOTE: Known bug, will return 'auto' if style value is 'auto'
-  const win = el.ownerDocument.defaultView
-  // null means not to return pseudo styles
-  return win.getComputedStyle(el, null)[property]
-}
 </script>
 
 <style lang="stylus">
